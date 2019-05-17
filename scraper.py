d110ab3a74911ee4160abaee727c679b6a484278
scpiano <sc.piano94@gmail.com>
2019-05-16 23:45:47 -0700

    renaming and more slight changes

--- scraper.py
+++ scraper.py
@@ -1,14 +1,18 @@
 import datetime
 import os
 import requests
-import requests-oauthlib
 from uuid import uuid4
 
-def auth_request():
-    client_id = ''
+def auth_request(): # Authorization required
+    client_id = '' # need to find better way to store this
     state = str(uuid4())
     redirect_uri = 'http://localhost:8080'
-    duration = 'temporary'
+    duration = 'temporary' # just for initial testing, need to change to permanent
     scope = 'read'
 
     auth_url = 'https://www.reddit.com/api/v1/authorize?client_id={}&response_type=code&state={}&redirect_uri={}&duration={}&scope={}'.format(client_id,state,redirect_uri,duration,scope)
+    print(auth_url) # testing
+    # requests.get(auth_url)
+
+if __name__ == '__main__'
+    auth_request()
