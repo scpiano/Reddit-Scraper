5ecd4bc55f9d2bc990a3c6ed7e0d61635e4e74cf
scpiano <sc.piano94@gmail.com>
2019-05-20 22:07:36 -0700

    updating gitignore and adding some more, along with interfacing with json file

--- scraper.py
+++ scraper.py
@@ -1,22 +1,31 @@
 import datetime
 import os
 import requests
-from uuid import uuid4
-
-def make_request(subreddits=['buildapcsales','hardwareswap'], search_term='[USA-WA]', limit=10, sort='new'):  
+import json
+    
+def make_request(*args):  
     """
     Takes a string or list of strings for subreddits requested, defaulting to buildapcsales and hardwareswap.
     Also takes in a string or list of strings for search term, defaulting to '[USA-WA]' for the US state of Washington, a limit
-    on the data requested and a sorting.
+    on the data requested and a sorting that can be one of the following: hot, new, comments, relevance or top.
     """
-    base_url = ''
+    with open('config.json') as config_json:
+        config = json.load(config_json)
+    
+    user_agent = {'user-agent':config['user_agent']}
+    subreddits = config['subreddits']
+    search_term = config['search_term']
+    limit = config['limit']
+    sort = config['sort']
 
     for subreddit in subreddits:
+        print(subreddit)
         base_url = 'https://www.reddit.com/r/{}/search.json?restrict_sr=1&q={}&limit={}&sort={}'.format(subreddit, search_term, limit, sort) 
+
         try:
             print(base_url)
-            API_request = requests.get(base_url)
-            print(API_request)
+            api_request = requests.get(base_url, headers = user_agent)
+            print(api_request.json())
         except Exception as e:
             print('Could not complete request:{}'.format(e))
 
